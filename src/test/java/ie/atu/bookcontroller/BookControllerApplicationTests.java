661d8c56a4913bf6b211cb1e004aee45747d40f3
Rory O Loughlin <g00422319@atu.ie>
2024-12-29 16:33:11 +0000

    committing project

---
+++ src/test/java/ie/atu/bookcontroller/BookControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.bookcontroller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookControllerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
